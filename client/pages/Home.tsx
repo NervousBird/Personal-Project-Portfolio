3059574e883bc388a77eaca8ab7f712e76dd0133
leo-walton <leowaltondesign@gmail.com>
2025-11-29 17:06:33 +1300

    chore: resolve small issues found by gemini

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,21 +1,20 @@
-import { Link } from "react-router"
+import { Link } from "react-router-dom"
 
 function Home() {
   const titleArray = "NervousBird Art".split("")
 
   return (
     <div className="home-container">
-      {/* <h1>NervousBird Art</h1> */}
       <div className="title-container">
         {titleArray.map((letter, idx) => (
-          <h1 key={idx} className='letter'>{letter}</h1>
+          <h1 key={`${letter}-${idx}`} className="letter">{letter}</h1>
         ))}
       </div>
       <section className="nav">
-        <Link to='/portfolio' viewTransition>Portfolio .</Link>
-        <Link to='/about' viewTransition>About .</Link>
-        <Link to='/contact' viewTransition>Contact .</Link>
-        <Link to='/websites' viewTransition>Websites .</Link>
+        <Link to="/portfolio" viewTransition>Portfolio .</Link>
+        <Link to="/about" viewTransition>About .</Link>
+        <Link to="/contact" viewTransition>Contact .</Link>
+        <Link to="/websites" viewTransition>Websites .</Link>
       </section>
     </div>
   )
