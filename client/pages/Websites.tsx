4e17388c14c8d7705a356d7bd1c6f582cb2ad1c8
leo-walton <leowaltondesign@gmail.com>
2025-12-11 20:50:38 +1300

    Add env to hide emailjs keys

--- client/pages/Websites.tsx
+++ client/pages/Websites.tsx
@@ -1,8 +1,22 @@
+import Nav from "../components/layout/Nav"
 
 function Websites() {
+  const titleArray = "Websites".split("")
+
   return (
-    <>
-    </>
+    <main className="websites-container">
+      <header>
+        {titleArray.map((letter, idx) => (
+          <h1 key={`${letter}-${idx}`}>{letter}</h1>
+        ))}
+      </header>
+
+      <Nav />
+
+      <section className="websites">
+        
+      </section>
+    </main>
   )
 }
 
