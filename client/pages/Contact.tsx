f19131cf635d7e6f34a2bc26aa63d50eec7ce509
leo-walton <leowaltondesign@gmail.com>
2025-12-16 10:18:28 +1300

    Add warning messages and some responsiveness

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -9,7 +9,8 @@ const keyId = import.meta.env.VITE_PUBLIC_KEY
 function Contact() {
   const titleArray = "Contact".split("")
   const form = useRef<string | HTMLFormElement>()
-  const [warning, setWarning] = useState(false)
+  const [notification, setNotification] = useState(false)
+  const [message, setMessage] = useState('')
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -23,12 +24,11 @@ function Contact() {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
-    console.log('submit')
-
     const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)
 
     if(formData.name === "" || !validEmail || formData.message === "") {
-      setWarning(true)
+      setMessage('Fill in ALL the form, correctly.')
+      setNotification(true)
     } else {
       // eslint-disable-next-line promise/catch-or-return
       emailjs
@@ -37,28 +37,30 @@ function Contact() {
         })
         .then(
           () => {
-            console.log('SUCCESS!')
+            setMessage('Thanks for getting in contact, I will reply when I feel like it.')
+            setFormData({name: '', email: '', message: ''})
+            setNotification(true)
+            setTimeout(() => {
+              setNotification(false)
+            }, 5000)
           },
           (error) => {
             console.log('FAILED...', error.text)
+            setMessage('Something went wrong (oh no!), please try again.')
+            setNotification(true)
           },
         )
-      
-      setFormData({name: '', email: '', message: ''})
-      setWarning(false)
     }
   }
 
   const handleReset = (e: FormEvent) => {
     e.preventDefault()
-    console.log('reset')
     setFormData({name: '', email: '', message: ''})
-    setWarning(false)
+    setNotification(false)
   }
 
   return (
     <main className="contact-container">
-      {/* <div className="background-image" /> */}
       <header>
         {titleArray.map((letter, idx) => (
           <h1 key={`${letter}-${idx}`}>{letter}</h1>
@@ -74,37 +76,49 @@ function Contact() {
           </span>
         </section>
 
-        {warning && <p>Fill in the form completely</p>}
-
         <section className="contact-form-container">
+
           <form ref={form} onSubmit={handleSubmit}>
             <span className="window-header">
               <div className="button">_</div>
               <div className="button">O</div>
               <div className="button">X</div>
             </span>
+            
             <span>
-              Please make sure your name and email are correct so that communications can proceed without issue.
+              <p>Please make sure your name and email are correct so that communications can proceed without issue.</p>
             </span>
+
             <span className="name-email">
               <label htmlFor="name">Name:</label>
               <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
               <label htmlFor="email">Email:</label>
               <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
             </span>
+
             <span className="message">
               <label htmlFor="message">Message:</label>
               <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
             </span>
+
             <span className="joke-message">
               Your information will be saved and sold to the highest bidder.* This is a joke.**
             </span>
+
             <span className="button-container">
               <button type="submit" value="Send">SEND</button>
               <span className="pps">**this isn't a joke</span>
               <button onClick={handleReset}>RESET</button>
             </span>
+
           </form>
+
+          {notification &&
+            <span className="notification">
+              <p>{message}</p>
+            </span>
+          }
+
         </section>
       </section>
     </main>
