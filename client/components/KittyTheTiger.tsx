5ffa58cf6c759c6f254e884680ceb2e8470ca153
leo-walton <leowaltondesign@gmail.com>
2026-08-14 14:18:19 +1200

    update projects art

--- client/components/KittyTheTiger.tsx
+++ client/components/KittyTheTiger.tsx
@@ -23,10 +23,10 @@ function KittyTheTiger() {
           <img className="image-3" src={"/kittythetiger/vis_dev_02.jpg"} />
           <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
         </div>
-        <div className="image-block">
-          <img className="image-4" src={"/kittythetiger/vis_dev_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
+        {/* <div className="image-block"> */}
+        {/*   <img className="image-4" src={"/kittythetiger/vis_dev_01.jpg"} /> */}
+        {/*   <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+        {/* </div> */}
       </section>
       <section>
         <h3>People</h3>
@@ -43,51 +43,51 @@ function KittyTheTiger() {
           <img className="image-3" src={"/kittythetiger/char_designs_03.jpg"} />
           <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
         </div>
-        <div className="image-block">
-          <img className="image-4" src={"/kittythetiger/char_designs_02.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
+        {/* <div className="image-block"> */}
+        {/*   <img className="image-4" src={"/kittythetiger/char_designs_02.jpg"} /> */}
+        {/*   <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+        {/* </div> */}
       </section>
-      <section>
-        <h3>Cars</h3>
-        <h4>Blurb about the cars and vehicles in the world, because that is just very Cyberpunk and cool.</h4>
-        <div className="image-block">
-          <img className="image-1" src={"/kittythetiger/car_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-        <div className="image-block">
-          <img className="image-2" src={"/kittythetiger/car_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-        <div className="image-block">
-          <img className="image-3" src={"/kittythetiger/car_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-        <div className="image-block">
-          <img className="image-4" src={"/kittythetiger/car_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-      </section>
-      <section>
-        <h3>Technology</h3>
-        <h4>Blurb about the technology in the world, maybe things that are positive, that could be achieved with technical advancements.</h4>
-        <div className="image-block">
-          <img className="image-1" src={"/kittythetiger/char_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-        <div className="image-block">
-          <img className="image-2" src={"/kittythetiger/env_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-        <div className="image-block">
-          <img className="image-3" src={"/kittythetiger/char_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-        <div className="image-block">
-          <img className="image-4" src={"/kittythetiger/char_designs_01.jpg"} />
-          <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p>
-        </div>
-      </section>
+      {/* <section> */}
+      {/*   <h3>Cars</h3> */}
+      {/*   <h4>Blurb about the cars and vehicles in the world, because that is just very Cyberpunk and cool.</h4> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-1" src={"/kittythetiger/car_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-2" src={"/kittythetiger/car_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-3" src={"/kittythetiger/car_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-4" src={"/kittythetiger/car_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/* </section> */}
+      {/* <section> */}
+      {/*   <h3>Technology</h3> */}
+      {/*   <h4>Blurb about the technology in the world, maybe things that are positive, that could be achieved with technical advancements.</h4> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-1" src={"/kittythetiger/char_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-2" src={"/kittythetiger/env_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-3" src={"/kittythetiger/char_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/*   <div className="image-block"> */}
+      {/*     <img className="image-4" src={"/kittythetiger/char_designs_01.jpg"} /> */}
+      {/*     <p>Blurb about the world of Kitty the Tiger, it's cyberpunk, but looking for the positive in it.</p> */}
+      {/*   </div> */}
+      {/* </section> */}
     </main>
   )
 }
